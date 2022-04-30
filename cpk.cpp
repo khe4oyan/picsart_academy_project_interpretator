45fb9c66ee00df483657cfa0897d9bd2fad67d05
khe4oyan <khe4oyan@bk.ru>
2022-04-30 18:45:19 +0400

    add default mode

--- cpk.cpp
+++ cpk.cpp
@@ -1,87 +1,178 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <string>
+#include <map>
+#include <Windows.h>
 
-enum mods{DEFAULT, CYCLE, CONDITION, SKIP};
+enum mods { DEFAULT, CYCLE, CONDITION, SKIP };
 
-std::string VARIABLES;
-std::fstream CODE ("variables.cpk");
-int line_pointer = 1; // for easy errors show line
-mods READ_MODE = DEFAULT;
-std::string CODE_LINE;
+struct Var {
+	std::string type;
+	std::string value;
+};
+
+std::fstream CODE("variables.cpk"); // path for programm code file
+std::string TOCKENS[4]; // every line devided in tockens
+std::string CODE_LINE; // get new line 
+std::map<std::string, Var> VARIABLES; // all variables with their value and type
+int line_pointer = 0; // for easy errors show line
+mods READ_MODE = DEFAULT; 
 
 #include "tools.h"
+void skip_mode();
 
-std::string get_first_key() {
-	std::string fk;
-
-	for (int i = 0; i < CODE_LINE.length(); ++i) {
-		if (CODE_LINE[i] == ' ' || CODE_LINE[i] == '}') {
-			break;
-		}
-		fk += CODE_LINE[i];
+void cnt_else_version() {
+	skip_mode();
+	GNL();
+	if (TOCKENS[0] == "else") {
+		READ_MODE = CONDITION;
 	}
-
-	return fk;
 }
 
-void find_variable(std::string varName) {
+void cnt_type_equal(std::string type) {
+	if (VARIABLES[TOCKENS[1]].type != type || VARIABLES[TOCKENS[3]].type != type) {
+		error("you can use this operator only with num types");
+	}
+}
+
+void cnt_big() { // >
+	cnt_type_equal("num");
+
+	// do..
+	if (std::stoi(VARIABLES[TOCKENS[1]].value) > std::stoi(VARIABLES[TOCKENS[3]].value)) {
+		READ_MODE = CONDITION;
+	}
+	else {
+		cnt_else_version();
+	}
+}
+
+void cnt_small() { // <
+	cnt_type_equal("num");
+
+	if (std::stoi(VARIABLES[TOCKENS[1]].value) < std::stoi(VARIABLES[TOCKENS[3]].value)) {
+		READ_MODE = CONDITION;
+	}
+	else {
+		cnt_else_version();
+	}
+}
+
+void cnt_equal() { // == 
+	if (VARIABLES[TOCKENS[1]].value == VARIABLES[TOCKENS[3]].value) {
+		READ_MODE = CONDITION;
+	}
+	else {
+		cnt_else_version();
+	}
+}
+void cnt_not_equal() { // != 
+	if (VARIABLES[TOCKENS[1]].value != VARIABLES[TOCKENS[3]].value) {
+		READ_MODE = CONDITION;
+	}
+	else {
+		cnt_else_version();
+	}
+}
+void cnt_and() { // && 
+}
+void cnt_or() { // || 
+}
+
+void cnt_opeartors(std::string opr) {
+	// >, <, ==, !=, &&, ||
 	
+	if(opr == ">") { cnt_big(); } else
+	if(opr == "<") { cnt_small(); } else
+	if(opr == "==") { cnt_equal(); } else
+	if(opr == "!=") { cnt_not_equal(); } else
+	if(opr == "&&") { cnt_and(); } else
+	if(opr == "||") { cnt_or(); } else
+	error("undefined condition operator");
 }
 
-void try_variable_create() {
-	// check have this variable or not
-	std::string tockens[3];
-	int index = 0;
-
-	for (int i = 0; i < CODE_LINE.length(); ++i) {
-		if (CODE_LINE[i] == ';') {break;}
-		if (CODE_LINE[i] == ' ') {index++;}
-		tockens[index] += CODE_LINE[i];
+void condition() {
+	std::string left_var = TOCKENS[1];
+	std::string right_var = TOCKENS[3];
+	if (!var_found(left_var) || !var_found(right_var)) {
+		error("this variable is exists");
 	}
 
-	// find variable name tockens[1] in VARIABLES
-	find_variable(tockens[1]);
+	if (VARIABLES[left_var].type != VARIABLES[right_var].type) {
+		error("condition values types isn't equal");
+	}
+
+	cnt_opeartors(TOCKENS[2]);
+
+/*
+	GNL();
+	if (TOCKENS[0] == "else") {
+		READ_MODE = SKIP;
+	}
+*/
+}
+
+void create_var() {
+	if (var_found(TOCKENS[1])) {
+		error("this name is exists");
+	}
+
+	if (TOCKENS[0] == "num") {
+		for (int i = 0; i < TOCKENS[2].length(); ++i) {
+			if (TOCKENS[2][i] >= 48 && TOCKENS[2][i] <= 57) {
+				continue;
+			}
+			error("you can't appropriate symbol for number type");
+		}
+	}
+
+	VARIABLES[TOCKENS[1]] = { TOCKENS[0], TOCKENS[2] };
 }
 
 void default_mode() {
 	if (CODE_LINE.length() == 0) { return; }
 	//std::cout << "Get line: " << CODE_LINE << "\n";
 
-	std::string first_keyword = get_first_key();
+	std::string first_keyword = TOCKENS[0];
 
-	std::cout << "- Keyword: " << first_keyword << std::endl;
-	if (first_keyword == "num" || first_keyword == "str" || first_keyword == "bool") {
-		try_variable_create();
-	}else
-	if (first_keyword == "if") {
-		// if logic function
-	}else
-	if(first_keyword == "for") {
-		// for logic function
+	if (TOCKENS[1] != "=") {
+		if (first_keyword == "num" || first_keyword == "str" || first_keyword == "bool") { create_var(); }else
+		if (first_keyword == "if") { condition(); }else
+		if (first_keyword == "for") { READ_MODE = SKIP; /* временно */ } else
+		if (first_keyword == "else") { READ_MODE = SKIP; }else
+		if (first_keyword == "output") { }
+		else { error("syntax error"); }
 	}
-	if (first_keyword == "else") {
-		READ_MODE = SKIP;
+	else {
+		// присвоение 
+		error("in developing..");
 	}
 }
-
+ 
 void cycle_mode() {
 
 }
 
 void condition_mode() {
+	while (CODE_LINE != "}") {
+		default_mode();
+		GNL();
+	}
+	GNL();
 
+	if (CODE_LINE == "else;") { READ_MODE = SKIP; }
+	else { READ_MODE = DEFAULT; }
 }
 
 void skip_mode() {
 	while (CODE_LINE != "}") {
 		GNL();
 	}
-	
+
 	READ_MODE = DEFAULT;
 }
 
-void line_read(){
+void line_read() {
 	while (!CODE.eof()) {
 		GNL();
 		if (READ_MODE == DEFAULT) {
@@ -96,11 +187,9 @@ void line_read(){
 		else {
 			condition_mode();
 		}
-
-		line_pointer++;
 	}
 }
 
-int main(){
+int main() {
 	line_read();
 }
--- tools.h
+++ tools.h
@@ -1,8 +1,20 @@
 #pragma once
-void first_w_space_del();
+void first_white_space_del();
 void GNL();
 void error(std::string msg = "undefined behaviour");
 void warning(std::string msg);
+void tockens_reset();
+void tockens_show();
+void get_tockens();
+void color_set(int color);
+
+template<typename T>
+void w(T t) {
+	std::cout << t;
+	color_set(8);
+	std::cout << " (" << line_pointer << " line)\n";
+	color_set(15);
+}
 
 void first_white_space_del() {
 	while (CODE_LINE[0] == '\t' || CODE_LINE[0] == ' ') {
@@ -10,17 +22,108 @@ void first_white_space_del() {
 	}
 }
 
+bool var_found(std::string var) {
+	return VARIABLES.count(var);
+}
+
 void GNL() {// Get Next Line
 	std::getline(CODE, CODE_LINE);
-	first_white_space_del();
 	line_pointer++;
+
+	first_white_space_del();
+	get_tockens();
+}
+
+void color_set(int color) {
+	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(h, color);
 }
 
 void error(std::string msg) {
-	std::cout << "ERROR in " << line_pointer << " line: " << msg << std::endl;
+	color_set(124);
+	std::cout << " ERROR ";
+	color_set(79);
+	std::cout << " in " <<  line_pointer << " line: " << msg << " " << std::endl;
+	color_set(15);
 	exit(0);
 }
 
 void warning(std::string msg) {
-	std::cout << "WARNING in " << line_pointer << " line: " << msg << std::endl;
+	color_set(142);
+	std::cout << " WARNING ";
+	color_set(232);
+	std::cout << " in " << line_pointer << " line: " << msg << " " << std::endl;
+	color_set(15);
+}
+
+void tockens_reset() {
+	for (int i = 0; i < 4; ++i) {
+		TOCKENS[i] = "";
+	}
+}
+
+void tockens_show() {
+	w("=====");
+	for (int i = 0; i < 4; ++i) {
+		w(TOCKENS[i]);
+	}
+}
+
+void get_string(int i) {
+	if (CODE_LINE[i] == '\'') {
+		i++;
+		while (CODE_LINE[i] != '\'') {
+			TOCKENS[2] += CODE_LINE[i++];
+		}
+	}
+	else if(CODE_LINE[i] >= 65 && CODE_LINE[i] <= 90 || CODE_LINE[i] >= 97 && CODE_LINE[i] <= 122){
+		while (CODE_LINE[i] != ';') {
+			TOCKENS[2] += CODE_LINE[i++];
+		}
+
+		if (var_found(TOCKENS[2])) {
+			if (VARIABLES[TOCKENS[2]].type != "str") {
+				error("reference variable type is not equal for " + TOCKENS[1] + " type");
+			}
+			TOCKENS[2] = VARIABLES[TOCKENS[2]].value;
+		}
+		else {
+			error("this variable is not found");
+		}
+
+	}
+	else {
+		error("variable keywork first char can was char");
+	}
+}
+
+void get_tockens() {
+	std::string fk;
+	int index = 0;
+
+	tockens_reset();
+
+	for (int i = 0; i < CODE_LINE.length(); ++i) {
+		if (index >= 4) {error("Max tockens is 4");}
+		
+		if(TOCKENS[0] == "str") {
+			if (index == 2) {
+				get_string(i);
+				w(TOCKENS[2]);
+				break;
+			}
+		}
+
+		if (CODE_LINE[i] == ' ' || CODE_LINE[i] == ';') {
+			TOCKENS[index] = fk;
+			fk = "";
+			index++;
+			continue;
+		}
+		fk += CODE_LINE[i];
+	}
+	
+	if (TOCKENS[3] != "" && TOCKENS[0] != "if") {
+		error("last word is undefined");
+	}
 }
